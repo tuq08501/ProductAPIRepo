851816436153a7a3d870cd4811de79324ad58d35
mamuntameema <143912915+mamuntameema@users.noreply.github.com>
2025-04-15 00:44:19 -0400

    lab 12

--- ProductsApp/ProductsApp/Controllers/ProductsController.cs
+++ ProductsApp/ProductsApp/Controllers/ProductsController.cs
@@ -11,9 +11,9 @@ namespace ProductsApp.Controllers
     {
         Product[] products = new Product[]
         {
-            new Product { Id = 1, Name = "Tomato Soup", Category = "Groceries", Price = 1 },
-            new Product { Id = 2, Name = "Yo-yo", Category = "Toys", Price = 3.75M },
-            new Product { Id = 3, Name = "Hammer", Category = "Hardware", Price = 16.99M }
+            new Product { Id = 1, Name = "Potato Soup", Category = "Schmoceries", Price = 42 },
+            new Product { Id = 2, Name = "Chicken", Category = "Food", Price = 2.75M },
+            new Product { Id = 3, Name = "Makeup", Category = "Beauty", Price = 17.99M }
         };
 
         public ProductsController()
